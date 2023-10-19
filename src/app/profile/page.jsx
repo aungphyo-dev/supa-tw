4691891aa3edf791cd2b5dab3a84513096468830
typle <aungpyaephyo1412@gmail.com>
2023-10-19 18:43:04 +0630

    user profile

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import {BiArrowBack} from "react-icons/bi";
+import {FaCalendarAlt} from "react-icons/fa";
 
 const Profile = () => {
     return (
@@ -18,6 +19,26 @@ const Profile = () => {
                 </div>
                 <div className='w-full'>
                     <div className="w-full h-[240px] bg-white"></div>
+                    <div className='w-full px-2 md:px-5 mt-[-75px] mb-3 flex justify-between items-end'>
+                        <div className='w-[150px] h-[150px] rounded-full bg-gray-500'></div>
+                        <button className='text-white px-5 py-2 rounded-full border border-gray-600 mb-3'>
+                            Edit profile
+                        </button>
+                    </div>
+                    <div className="w-full px-2 md:px-5">
+                        <div className='w-full flex flex-col'>
+                            <h1 className='text-white font-semibold text-xl'>Typle</h1>
+                            <p className='text-sm text-gray-400'>@typle</p>
+                        </div>
+                        <div className="w-full mt-3">
+                            <div className='w-fit flex text-gray-400 text-sm justify-center items-center gap-x-2'>
+                                <FaCalendarAlt/>
+                                <div>
+                                    Joined August 2023
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="w-full px-2 py-5 md:px-5 min-h-screen"></div>
             </div>
