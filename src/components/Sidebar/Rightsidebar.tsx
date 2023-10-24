878f85e1887f69cb17e68bae9ed740f994858fd2
typle <aungpyaephyo1412@gmail.com>
2023-10-24 15:10:11 +0630

    right side

--- src/components/Sidebar/Rightsidebar.tsx
+++ src/components/Sidebar/Rightsidebar.tsx
@@ -1,20 +1,34 @@
+"use client"
 import {BsSearch} from "react-icons/bs";
+import {useQuery} from "@tanstack/react-query";
+import { UserCard} from "~/components";
+import AXIOSC from "~/services/AXIOSC";
 
 const Rightsidebar = () => {
+    const {data} = useQuery({
+        queryKey: ["get", "auth", "users"],
+        queryFn: async () => {
+            const res = await AXIOSC.get("/auth/users")
+            return res.data
+        }
+    })
     return (
-        <div className="w-full flex flex-col sticky top-0 h-screen">
+        <div className="w-full flex flex-col sticky top-0 h-screen overflow-hidden px-5">
             <div className="w-full">
-                <div className="p-5 flex w-full">
+                <div className="py-5 flex w-full">
                     <label htmlFor="search">
                         <BsSearch className='text-xl text-white absolute left-[35px] top-[36px]'/>
                     </label>
                     <input type="text" id='search' placeholder="Search" className='pl-[45px] placeholder:text-gray-400 w-full outline-none bg-[#202327] focus:bg-transparent rounded-full border border-[#202327] focus:border-blue-500 py-3'/>
                 </div>
             </div>
-            <div className="w-full  px-5 text-white">
+            <div className="w-full   text-white mb-4">
                 <div className="w-full">
-                    <h1 className='text-white font-semibold text-lg'>Trends for you</h1>
+                    <h1 className='text-white font-semibold text-lg'>Who to follow you?</h1>
                 </div>
+            </div>
+            <div className="w-full flex-1 flex flex-col gap-y-2 bg-[#202327] rounded">
+                {data?.users?.data?.map((user:{id:number, user : any}) => <UserCard key={user?.id} user={user}/>)}
             </div>
         </div>
     );
