43480d80dbe5d302fb3512eed3f28e402a0f120d
typle <aungpyaephyo1412@gmail.com>
2023-10-26 08:21:48 +0630

    user profile fix

--- src/components/Partials/UserProfile.tsx
+++ src/components/Partials/UserProfile.tsx
@@ -1,91 +1,92 @@
-import React from 'react';
+import React from "react";
 import Link from "next/link";
-import {BiArrowBack} from "react-icons/bi";
-import {LoadingCircle} from "~/components";
 import Image from "next/image";
-import {CiLocationOn} from "react-icons/ci";
-import {AiOutlineLink} from "react-icons/ai";
-import {FaCalendarAlt} from "react-icons/fa";
+import { BiArrowBack } from "react-icons/bi";
+import { LoadingCircle } from "~/components";
+import { CiLocationOn } from "react-icons/ci";
+import { AiOutlineLink } from "react-icons/ai";
+import { FaCalendarAlt } from "react-icons/fa";
 
-const UserProfile = ({user,isLoading}:{user:any,isLoading:boolean}) => {
-    return (
-        <>
-            <div
-                className='font-semibold text-gray-50 text-xl sticky top-0 bg-black z-[100000] backdrop-blur bg-black/80 border-b-[0.5px] border-b-gray-600 py-3 px-2 md:px-5'>
-                <div className='w-fit flex justify-center items-center gap-x-2'>
-                    <Link href={"/"} className='text-xl text-white hover:bg-white/20 p-3 rounded-full'>
-                        <BiArrowBack/>
-                    </Link>
-                    <div>
-                        <div>
-                            {isLoading ? <LoadingCircle/> : user?.name}
-                        </div>
-                        <p className='text-sm text-gray-400'>{user?.tweets?.length} Post</p>
-                    </div>
-                </div>
+const UserProfile = ({
+  user,
+  isLoading,
+}: {
+  user: any;
+  isLoading: boolean;
+}) => {
+  return (
+    <>
+      <div className="font-semibold text-gray-50 text-xl sticky top-0 bg-black z-[100000] backdrop-blur bg-black/80 border-b-[0.5px] border-b-gray-600 py-3 px-2 md:px-5">
+        <div className="w-fit flex justify-center items-center gap-x-2">
+          <Link
+            href={"/"}
+            className="text-xl text-white hover:bg-white/20 p-3 rounded-full"
+          >
+            <BiArrowBack />
+          </Link>
+          <div>
+            <div>{isLoading ? <LoadingCircle /> : user?.name}</div>
+            <p className="text-sm text-gray-400">{user?.tweets} Post</p>
+          </div>
+        </div>
+      </div>
+      <div className="w-full">
+        <div className="w-full h-[240px] bg-gray-500"></div>
+        <div className="w-full px-2 md:px-5 mt-[-75px] mb-3 flex justify-between items-end">
+          <div className="w-[150px] h-[150px] border-2 border-black rounded-full bg-gray-500 relative overflow-hidden">
+            <Image
+              src={!user?.profile_avatar ? "/avatar.jpg" : user?.profile_avatar}
+              alt={"Profile"}
+              className="object-cover"
+              priority
+              fill
+              sizes={"500"}
+            />
+          </div>
+          <button className="text-white px-5 py-2 rounded-full border border-gray-600 mb-3">
+            Edit profile
+          </button>
+        </div>
+        <div className="w-full px-2 md:px-5">
+          <div className="w-full flex flex-col">
+            <h1 className="text-white font-semibold text-xl">
+              {isLoading ? <LoadingCircle /> : user?.name}
+            </h1>
+            <p className="text-sm text-gray-400">@{user?.username}</p>
+            {user?.bio && (
+              <p className="text-sm text-gray-400 mt-4">{user?.bio}</p>
+            )}
+          </div>
+          <div className="w-full flex gap-3 flex-wrap my-3">
+            {user?.location && (
+              <div className="w-fit flex text-gray-400 text-sm justify-center items-center gap-x-2">
+                <CiLocationOn />
+                <div>{user?.location}</div>
+              </div>
+            )}
+            {user?.website && (
+              <div className="w-fit flex text-sm justify-center items-center gap-x-2">
+                <AiOutlineLink className="text-gray-400" />
+                <Link
+                  href={user?.website}
+                  className="text-blue-500 hover:underline "
+                  target={"_blank"}
+                >
+                  {user?.website}
+                </Link>
+              </div>
+            )}
+            <div className="w-fit flex text-gray-400 text-sm justify-center items-center gap-x-2">
+              <FaCalendarAlt />
+              <div>
+                Joined {isLoading ? <LoadingCircle /> : user?.created_at}
+              </div>
             </div>
-            <div className='w-full'>
-                <div className="w-full h-[240px] bg-gray-500"></div>
-                <div className='w-full px-2 md:px-5 mt-[-75px] mb-3 flex justify-between items-end'>
-                    <div className='w-[150px] h-[150px] border-2 border-black rounded-full bg-gray-500 relative overflow-hidden'>
-                        <Image src={!user?.profile_avatar ? "/avatar.jpg" : user?.profile_avatar}
-                               alt={"Profile"}
-                               className='object-cover'
-                               priority
-                               fill
-                               sizes={"500"}
-                        />
-                    </div>
-                    <button className='text-white px-5 py-2 rounded-full border border-gray-600 mb-3'>
-                        Edit profile
-                    </button>
-                </div>
-                <div className="w-full px-2 md:px-5">
-                    <div className='w-full flex flex-col'>
-                        <h1 className='text-white font-semibold text-xl'>{isLoading ? <LoadingCircle/> : user?.name}</h1>
-                        <p className='text-sm text-gray-400'>
-                            {user?.username}
-                        </p>
-                        {
-                            user?.bio && <p className='text-sm text-gray-400 mt-4'>{user?.bio}</p>
-                        }
-                    </div>
-                    <div className="w-full flex gap-3 flex-wrap my-3">
-                        {
-                            user?.location && <div className='w-fit flex text-gray-400 text-sm justify-center items-center gap-x-2'>
-                                <CiLocationOn/>
-                                <div>
-                                    {user?.location}
-                                </div>
-                            </div>
-                        }
-                        {user?.website && <div
-                            className='w-fit flex text-sm justify-center items-center gap-x-2'>
-                            <AiOutlineLink className="text-gray-400"/>
-                            <Link href={user?.website} className="text-blue-500 hover:underline " target={"_blank"}>
-                                {user?.website}
-                            </Link>
-                        </div>}
-                        <div className='w-fit flex text-gray-400 text-sm justify-center items-center gap-x-2'>
-                            <FaCalendarAlt/>
-                            <div>
-                                Joined {isLoading ? <LoadingCircle/> : user?.created_at}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-full flex gap-3 flex-wrap my-3">
-                        <Link href={"/profile/following"} className='w-fit hover:underline flex text-gray-400 text-sm justify-center items-center gap-x-2'>
-                            {!isLoading && user?.followings.length + " following"}
-                        </Link>
-                        <Link href={"/profile/followers"} className='w-fit hover:underline flex text-gray-400 text-sm justify-center items-center gap-x-2'>
-                            {!isLoading && user?.followers.length + " follower"}
-                        </Link>
-                    </div>
-
-                </div>
-            </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UserProfile;
