20ec60a53e238cbbebe3aa1779fac5e67d1f01c1
typle <aungpyaephyo1412@gmail.com>
2023-10-26 08:23:16 +0630

    user card

--- src/components/Card/UserCard.tsx
+++ src/components/Card/UserCard.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 const UserCard = ({user}:{user : any}) => {
     return (
-        <div className='w-full flex justify-between items-center px-5 py-3  bg-[#202327]  hover:bg-white/20 cursor-pointer'>
+        <div className='w-full flex justify-between items-center px-5 py-3  hover:bg-white/20 cursor-pointer'>
             <div className='flex-1 flex justify-start items-start'>
                 <div className='w-[40px] h-[40px] relative rounded overflow-hidden'>
                     <Image fill sizes={"500"} src="/avatar.jpg" alt="" className='block w-full h-full'/>
                 </div>
-                <div className="ml-2">
-                    <Link href={`/profile/${user.id}`} className='text-sm text-white hover:underline'>{user.name}</Link>
-                    <p className='text-xs text-gray-300'>@{user.username}</p>
+                <div className="ml-2 max-w-[9rem] overflow-hidden">
+                    <Link href={`/profile/${user.id}`} className='text-sm text-white hover:underline truncate'>{user.name}</Link>
+                    <p className='text-xs text-gray-300 truncate'>@{user.username}</p>
                 </div>
             </div>
             <div>
