8a1f404b86f76ede5cd49147582ea0a1aa456e3e
typle <aungpyaephyo1412@gmail.com>
2023-10-26 08:22:16 +0630

    profile auth

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,28 +1,42 @@
-"use client"
-import {AuthLayout, UserProfile, UserTweetLayout} from "~/components";
-import {useQuery} from "@tanstack/react-query";
+"use client";
+import { AuthLayout, UserProfile, UserTweetLayout } from "~/components";
+import { useQuery } from "@tanstack/react-query";
 import AXIOSC from "~/services/AXIOSC";
-
+import Link from "next/link";
+import React from "react";
 
 const Profile = () => {
-    const {data, isLoading} = useQuery({
-        queryKey: ["get", "auth", "profile", "user"],
-        queryFn: async () => {
-            const res = await AXIOSC.get("/auth/profile")
-            return res.data
-        },
-
-    })
-    return (
-        <AuthLayout>
-            <main className='w-full md:border-x-[0.5px] border-gray-600'>
-                <div className="w-full min-h-screen">
-                    <UserProfile user={data?.user} isLoading={isLoading}/>
-                    <UserTweetLayout id={data?.user?.id} user={data?.user}/>
-                </div>
-            </main>
-        </AuthLayout>
-    );
+  const { data, isLoading } = useQuery({
+    queryKey: ["get", "auth", "profile", "user"],
+    queryFn: async () => {
+      const res = await AXIOSC.get("/auth/profile");
+      return res.data;
+    },
+  });
+  return (
+    <AuthLayout>
+      <main className="w-full md:border-x-[0.5px] border-gray-600">
+        <div className="w-full min-h-screen">
+          <UserProfile user={data?.user} isLoading={isLoading} />
+          <div className=" px-2 md:px-5 w-full flex gap-3 flex-wrap my-3">
+            <Link
+              href={"/profile/following"}
+              className="w-fit hover:underline flex text-gray-400 text-sm justify-center items-center gap-x-2"
+            >
+              {!isLoading && data?.user?.followings + " following"}
+            </Link>
+            <Link
+              href={"/profile/followers"}
+              className="w-fit hover:underline flex text-gray-400 text-sm justify-center items-center gap-x-2"
+            >
+              {!isLoading && data?.user?.followers + " follower"}
+            </Link>
+          </div>
+          <UserTweetLayout id={data?.user?.id} user={data?.user} />
+        </div>
+      </main>
+    </AuthLayout>
+  );
 };
 
 export default Profile;
