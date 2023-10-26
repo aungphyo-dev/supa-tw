6eb8aa6969d3b9cec28db8e756ae103dd6023dfa
typle <aungpyaephyo1412@gmail.com>
2023-10-26 08:22:26 +0630

    profile by id

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,30 +1,38 @@
-"use client"
-import {AuthLayout, UserProfile, UserTweetLayout} from "~/components";
-import {useQuery} from "@tanstack/react-query";
+"use client";
+import { AuthLayout, UserProfile, UserTweetLayout } from "~/components";
+import { useQuery } from "@tanstack/react-query";
 import AXIOSC from "~/services/AXIOSC";
-import {useParams} from "next/navigation";
-
+import { useParams } from "next/navigation";
+import React from "react";
 
 const UserProfileByID = () => {
-    const {id} = useParams()
-    const {data, isLoading} = useQuery({
-        queryKey: ["get", "auth", "profile", "user",id],
-        queryFn: async () => {
-            const res = await AXIOSC.get(`/auth/user/${id}`)
-            return res.data
-        }
-    })
+  const { id } = useParams();
+  const { data, isLoading } = useQuery({
+    queryKey: ["get", "auth", "profile", "user", id],
+    queryFn: async () => {
+      const res = await AXIOSC.get(`/auth/user/${id}`);
+      return res.data;
+    },
+  });
 
-    return (
-        <AuthLayout>
-            <main className='w-full md:border-x-[0.5px] border-gray-600'>
-                <div className="w-full min-h-screen">
-                    <UserProfile user={data?.user} isLoading={isLoading}/>
-                    <UserTweetLayout id={data?.user?.id} user={data?.user}/>
-                </div>
-            </main>
-        </AuthLayout>
-    );
+  return (
+    <AuthLayout>
+      <main className="w-full md:border-x-[0.5px] border-gray-600">
+        <div className="w-full min-h-screen">
+          <UserProfile user={data?.user} isLoading={isLoading} />
+          <div className=" px-2 md:px-5 w-full flex gap-3 flex-wrap my-3">
+            <div className="w-fit  flex text-gray-400 text-sm justify-center items-center gap-x-2">
+              {!isLoading && data?.user?.followings + " following"}
+            </div>
+            <div className="w-fit  flex text-gray-400 text-sm justify-center items-center gap-x-2">
+              {!isLoading && data?.user?.followers + " follower"}
+            </div>
+          </div>
+          <UserTweetLayout id={data?.user?.id} user={data?.user} />
+        </div>
+      </main>
+    </AuthLayout>
+  );
 };
 
 export default UserProfileByID;
